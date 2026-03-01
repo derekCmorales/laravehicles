dfc01be412ca5312dc8085453db5190ba19b762c
derekCmorales <derek.calderonmorales@gmail.com>
2026-03-01 00:38:52 -0600

    feat: add validation to CreatePropertyCertificateDto fields

--- src/vehicles/dto/propertyCertificate.dto.ts
+++ src/vehicles/dto/propertyCertificate.dto.ts
@@ -1,12 +1,33 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsDateString, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreatePropertyCertificateDto {
+  @IsString()
+  @IsNotEmpty()
   noCertificado: string;
+
+  @IsString()
+  @IsNotEmpty()
   codigoUnicoIdentificador: string;
+
+  @IsDateString()
+  @IsNotEmpty()
   fechaEmision: Date;
+
+  @IsString()
+  @IsNotEmpty()
   aduanaLiquidadora: string;
+
+  @IsString()
+  @IsNotEmpty()
   polizaImportacion: string;
+
+  @IsDateString()
+  @IsNotEmpty()
   fechaPoliza: Date;
+
+  @IsNumber()
+  @IsNotEmpty()
   franquiciaNo: number;
 }
 

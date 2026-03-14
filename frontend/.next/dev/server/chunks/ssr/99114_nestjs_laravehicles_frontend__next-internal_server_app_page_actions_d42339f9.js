f78a57b8fd6366651f8485d7fa5ba102c607ef5e
derekCmorales <derek.calderonmorales@gmail.com>
2026-03-13 20:53:25 -0600

    chore: untrack Next.js build output

--- frontend/.next/dev/server/chunks/ssr/99114_nestjs_laravehicles_frontend__next-internal_server_app_page_actions_d42339f9.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/DEREK-PROJECTS/PERSONAL/platzi/nestjs/laravehicles/frontend/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=99114_nestjs_laravehicles_frontend__next-internal_server_app_page_actions_d42339f9.js.map
